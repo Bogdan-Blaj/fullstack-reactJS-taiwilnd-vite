ec3e4491bf8d847e432af2c148dd069f40a1c396
Bogdan-Blaj <bogdanblaj.main@gmail.com>
2024-01-28 18:50:22 +0200

    Created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div></div>
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
     </>
   );
 }
