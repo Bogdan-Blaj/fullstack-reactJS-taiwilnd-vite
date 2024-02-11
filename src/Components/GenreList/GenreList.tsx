50933d1cb84d8a902ed2015a2a0c562b435a309d
Bogdan-Blaj <bogdanblaj.main@gmail.com>
2024-02-12 00:13:48 +0200

    display genre list to UI

--- src/Components/GenreList/GenreList.tsx
+++ src/Components/GenreList/GenreList.tsx
@@ -1,25 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import GlobalApi from "../../Services/GlobalApi";
 import { Genre, GenreResponse } from "./GenreList.types";
 import { AxiosResponse } from "axios";
 
 function GenreList() {
+  const [genreList, setGerneList] = useState<Genre[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   useEffect(() => {
     getGenreList();
   }, []);
 
+  useEffect(() => {
+    // Log the updated state after it has been set
+    console.log("genreList", genreList);
+  }, [genreList]); // Add genreList as a dependency to the useEffect
+
   const getGenreList = async () => {
     try {
       const resp: AxiosResponse<GenreResponse> = await GlobalApi.getGenreList();
-      const results: Genre[] = resp.data.results;
-      console.log(results);
+      setGerneList(resp.data.results);
     } catch (error) {
       // Handle error here
       console.error("Error fetching genre list:", error);
     }
   };
 
-  return <div>GenreList ---</div>;
+  return (
+    <div>
+      <h2 className="text-[30px] font-bold dark:text-white">Genre</h2>
+      {genreList.map((item, index) => (
+        <div
+          onClick={() => setActiveIndex(index)}
+          className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-600 p-2  rounded-lg group
+        ${activeIndex == index ? "bg-gray-300 dark:bg-gray-600" : null}`}
+        >
+          <img
+            src={item.image_background}
+            className={`w-[60px] h-[60px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out duration-300 
+            ${activeIndex == index ? "scale-105" : null}`}
+          />
+          <h3
+            className={`dark:white text-[18px] group-hover:font-bold transition-all ease-out duration-300 ${activeIndex == index ? "font-bold" : null}`}
+          >
+            {item.name}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default GenreList;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,7 @@ import GenreList from "../Components/GenreList/GenreList";
 function Home() {
   return (
     <div className="grid grid-cols-4">
-      <div className="bg-red-600 h-full hidden md:block">
+      <div className="h-full hidden md:block px-6">
         <GenreList />
       </div>
       <div className="col-span-34 md:col-span-3 bg-blue-400">Genre List</div>
